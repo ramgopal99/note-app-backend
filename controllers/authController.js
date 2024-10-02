7cf6765a674451280737ee58bf08fad48049b932
ramgopal <ramgopalbagh009@gmail.com>
2024-10-02 09:39:34 +0530

    c

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,17 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 
-const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
+// const generateToken = (user) => {
+//   return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+// };
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const user = new User({ username, email, password });
     await user.save();
-    const token = generateToken(user);
-    res.status(201).json({ token });
+    // const token = generateToken(user);
+    // res.status(201).json({ token });
   } catch (error) {
     res.status(400).json({ message: 'Failed to register user', error });
   }
@@ -22,10 +22,10 @@ export const login = async (req, res) => {
   console.log('Email:', email, 'Password:', password); // Check if these values are correct
   try {
     const user = await User.findOne({ email });
-    if (!user || !(await user.comparePassword(password))) {
+    if (!user ) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
-    const token = generateToken(user);
+    // const token = generateToken(user);
     res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ message: 'Failed to login', error });
